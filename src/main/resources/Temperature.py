4b3713a591b3865a7f42292d621d1164866e5c70
Nilay <nilaybhatt@my.ccsu.edu>
2016-11-20 14:35:55 -0500

    added a python file that gets the temperature

---
+++ src/main/resources/Temperature.py
@@ -0,0 +1,21 @@
+from grovepi import *
+from grove_rgb_lcd import *
+
+dht_sensor_port = 7  # Connect the DHt sensor to port 7
+dht_sensor_type = 0  # change this depending on your sensor type
+# Note the dht_sensor_type below may need to be changed depending on which DHT sensor you have:
+#  0 - DHT11 - blue one - comes with the GrovePi+ Starter Kit
+#  1 - DHT22 - white one, aka DHT Pro or AM2302
+#  2 - DHT21 - black one, aka AM2301
+
+try:
+    [temp, hum] = dht(dht_sensor_port, dht_sensor_type)  # Get the temperature and Humidity from the DHT sensor
+    print("temp =", temp, "C")
+    t = str(temp)
+    h = str(hum)
+
+    setRGB(0, 128, 64)
+    setRGB(0, 255, 0)
+    setText("Temp:" + t + "C      " + "Humidity :" + h + "%")
+except (IOError, TypeError) as e:
+    print("{0}".format(e))
