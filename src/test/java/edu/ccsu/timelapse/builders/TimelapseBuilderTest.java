bb4aafdad1fe6fd500bb794da974453daa4973e0
Lukasz Brodowski <lukebrodowski@gmail.com>
2016-12-01 17:06:03 -0500

    builder tests

---
+++ src/test/java/edu/ccsu/timelapse/builders/TimelapseBuilderTest.java
@@ -0,0 +1,13 @@
+package edu.ccsu.timelapse.builders;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimelapseBuilderTest {
+
+	/**
+	 * @TODO
+	 */
+	
+}
---
+++ src/test/java/edu/ccsu/timelapse/models/TimelapseTest.java
@@ -0,0 +1,12 @@
+package edu.ccsu.timelapse.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimelapseTest {
+	
+	/**
+	 * @TODO
+	 */
+}
