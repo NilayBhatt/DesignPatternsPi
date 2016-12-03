f1e76583eb9ca9b87ec44601394a4616adbf489e
Nilay <nilaybhatt@my.ccsu.edu>
2016-12-03 09:45:11 -0500

    Fixed Temperature sensor

--- src/main/java/edu/ccsu/timelapse/components/TemperatureSensor.java
+++ src/main/java/edu/ccsu/timelapse/components/TemperatureSensor.java
@@ -1,27 +1,40 @@
 package edu.ccsu.timelapse.components;
 
+import edu.ccsu.timelapse.adapters.CommandLineAdapter;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
-import edu.ccsu.timelapse.adapters.CommandLineAdapter;
-
+/**
+ * TemperatureSensor class that adapts to the python code
+ * The python code will be native and running on pi.
+ */
 public class TemperatureSensor extends CommandLineAdapter {
 
+	/**
+	 * Overrides the command() method to build the command.
+	 * @return String
+	 */
 	@Override
 	public String command() {
 		
 		return "python ./scripts/temperature.py";
 	}
 
+	/**
+	 * Gets the temperature from the
+	 * temperature sensor of the GrovePi
+	 * @return double temperature in fahrenheit.
+	 */
 	public double getTemperature(){
 		BufferedReader output = execute();
-		double tempInCelsius = 0;
+		double tempInFahrenheit = 0;
 		try {
-			tempInCelsius = Double.parseDouble(output.readLine());
+			tempInFahrenheit = Double.parseDouble(output.readLine());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return  tempInCelsius;
+		return  tempInFahrenheit;
 	}
 }
