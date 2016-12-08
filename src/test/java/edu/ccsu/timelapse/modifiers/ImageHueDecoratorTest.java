c2ec1678f61a3097d7560038620567d4c47191a4
Deepankar Malhan <deepankarmalhan@gmail.com>
2016-12-07 21:20:30 -0500

    Added JUnit tests for the Decorator pattern classes and the custom Date wrapper DateFormatted class.

--- src/test/java/edu/ccsu/timelapse/modifiers/ImageHueDecoratorTest.java
+++ src/test/java/edu/ccsu/timelapse/modifiers/ImageHueDecoratorTest.java
@@ -5,8 +5,21 @@
  */
 package edu.ccsu.timelapse.modifiers;
 
+import org.junit.Test;
+
+import edu.ccsu.timelapse.imagecollections.ImageComponent;
+import edu.ccsu.timelapse.imagecollections.ImageComposite;
+import org.junit.Assert;
+
 public class ImageHueDecoratorTest {
 	
-	
-
+	@Test
+	public void eequals() {
+		ImageComponent decorator = new ImageComposite();
+		
+		ImageHueDecorator hueDecorator1 = new ImageHueDecorator(decorator);
+		ImageHueDecorator hueDecorator2 = new ImageHueDecorator(decorator);
+		
+		Assert.assertTrue(hueDecorator1.equals(hueDecorator2));
+	}
 }
--- src/test/java/edu/ccsu/timelapse/modifiers/ImageTimeDecoratorTest.java
+++ src/test/java/edu/ccsu/timelapse/modifiers/ImageTimeDecoratorTest.java
@@ -5,12 +5,21 @@
  */
 package edu.ccsu.timelapse.modifiers;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import edu.ccsu.timelapse.imagecollections.ImageComponent;
+import edu.ccsu.timelapse.imagecollections.ImageComposite;
+
 public class ImageTimeDecoratorTest {
-    @Test
-    public void process() throws Exception {
-
-
-    }
+	
+	@Test
+	public void eequals() {
+		ImageComponent decorator = new ImageComposite();
+		
+		ImageTimeDecorator timeDecorator1 = new ImageTimeDecorator(decorator);
+		ImageTimeDecorator timeDecorator2 = new ImageTimeDecorator(decorator);
+		
+		Assert.assertTrue(timeDecorator1.equals(timeDecorator2));
+	}
 }
